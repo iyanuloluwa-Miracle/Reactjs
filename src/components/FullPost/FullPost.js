5e101de6d1a85c7032e27807a614e214795dd2b6
iyanuloluwa <iremide.nexzy@gmail.com>
2022-03-22 14:56:50 +0100

    reactjs commit message

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+import classes from './FullPost.css';
+
+class FullPost extends Component {
+
+    state = {
+        loadedPost:null
+    }
+    componentDidUpdate(){
+        if(this.props.id){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+                axios.get('/posts/' + this.props.id)
+                .then(response =>{
+                    //console.log(response)
+                    this.setState({loadedPost: response.data})
+                })
+
+            }
+           
+        }
+      
+    }
+    deletePostHandler = ()=>{
+        axios.delete('/posts/' + this.props.id)
+            .then(response =>{
+                console.log(response)
+            })
+
+    }
+    render () {
+
+        let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
+        if(this.props.id){
+            post = <p style={{textAlign:'center',color:'red'}}>Loading......</p>;
+
+        }
+        if(this.state.loadedPost){
+            post = (
+                <div className={classes.FullPost}>
+
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className={classes.Edit}>
+                        <button onClick={this.deletePostHandler} className={classes.Delete}>Delete</button>
+                    </div>
+                </div>
+    
+            );
+
+        }
+        
+        return post;
+    }
+}
+
+export default FullPost;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+
+
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request =>{
+  console.log(request);
+  return request
+},error =>{
+  console.log(error)
+  return Promise.reject(error)
+
+})
+
+
+axios.interceptors.response.use(response =>{
+  console.log(response);
+  return response
+},error =>{
+  console.log(error)
+  return Promise.reject(error)
+
+})
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
